9b4c2ab1d8b3cd4892b01d65c93ee96706d97b22
Ricardo Bomfim <ricardoub@gmail.com>
2017-06-18 18:47:45 -0300

    first commit

---
+++ src/app/menuh/menuh.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menuh',
+  templateUrl: './menuh.component.html',
+  styleUrls: ['./menuh.component.css']
+})
+export class MenuhComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
